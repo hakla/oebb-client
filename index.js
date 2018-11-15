07c16ed31974702b06a91b733ce2d52b04058ac8
hakla <info@hakla.at>
2018-11-15 20:44:21 +0100

    Implemented journey (basic version that works only with ids)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const { format, parse } = require("date-fns");
-const inquirer = require('inquirer')
+const inquirer = require("inquirer");
 const oebb = require("oebb-hafas");
 const vorpal = require("vorpal")();
 
@@ -13,7 +13,7 @@ function departures(id, cb, duration = 60) {
       date: parse(r.when),
       direction: r.direction,
       name: r.line.name,
-      when: format(parse(r.when), "HH:mm")
+      when: time(r.when)
     }));
 
     cb(mapped);
@@ -26,50 +26,65 @@ function find(name, cb) {
   });
 }
 
+function time(rawDate, pattern = "HH:mm") {
+  return format(parse(rawDate), pattern);
+}
+
 if (argv.length > 2) {
-    const getopts = require('getopts')
-    const options = getopts(process.argv.slice(2), {
-        alias: {
-            d: 'duration',
-            f: 'first'
-        }
-    })
-
-    const duration = options.duration || 60
-    const name = options._[0]
-    const takeFirstStation = options.first || false
-
-    if (!name) {
-        console.warn('Keine Station angegeben')
-    } else {
-        find(name, results => {
-            let result
-
-            const f = (id) => departures(id, results => {
-                results.forEach(_ => console.log(`${_.when}: ${_.name} nach ${_.direction}`))
-            }, duration)
-
-            if (results.length > 1) {
-                if (takeFirstStation) {
-                    f(results[0].id)
-                } else {
-                    inquirer.prompt([{
-                        choices: results.map(result => ({
-                            name: result.name,
-                            value: result
-                        })),
-                        message: 'Welche Station?',
-                        name: 'station',
-                        type: 'list',
-                    }]).then(result => {
-                        f(result.station.id)
-                    })
-                }
-            } else {
-                console.warn('Keine Stationen gefunden!')
-            }
-        })
+  const getopts = require("getopts");
+  const options = getopts(process.argv.slice(2), {
+    alias: {
+      d: "duration",
+      f: "first"
     }
+  });
+
+  const duration = options.duration || 60;
+  const name = options._[0];
+  const takeFirstStation = options.first || false;
+
+  if (!name) {
+    console.warn("Keine Station angegeben");
+  } else {
+    find(name, results => {
+      let result;
+
+      const f = id =>
+        departures(
+          id,
+          results => {
+            results.forEach(_ =>
+              console.log(`${_.when}: ${_.name} nach ${_.direction}`)
+            );
+          },
+          duration
+        );
+
+      if (results.length > 1) {
+        if (takeFirstStation) {
+          f(results[0].id);
+        } else {
+          inquirer
+            .prompt([
+              {
+                choices: results.map(result => ({
+                  name: result.name,
+                  value: result
+                })),
+                message: "Welche Station?",
+                name: "station",
+                type: "list"
+              }
+            ])
+            .then(result => {
+              f(result.station.id);
+            });
+        }
+      } else {
+        console.warn("Keine Stationen gefunden!");
+      }
+    });
+  }
 } else {
   vorpal.command("locations <name>").action((args, callback) => {
     find(args.name, locations => {
@@ -87,6 +102,46 @@ if (argv.length > 2) {
         console.log(
           results.map(_ => `${_.when}: ${_.name} nach ${_.direction}`)
         );
+
+        callback();
+      });
+    });
+
+  vorpal
+    .command("journey <from> <to>")
+    .option("-d, --descriptive", "Print way descriptions")
+    .alias("j")
+    .action((args, callback) => {
+      oebb.journeys(args.from.toString(), args.to.toString()).then(results => {
+        const way = results[0].legs.reduce(
+          (a, b) => a + " => " + b.destination.name,
+          results[0].legs[0].origin.name
+        );
+
+        results.forEach(result => {
+          const way = result.legs.reduce(
+            (a, b) => a + "  " + b.arrival + b.destination.name,
+            result.legs[0].departures + result.legs[0].origin.name
+          );
+
+          console.log(
+            time(result.legs[0].departure) +
+              " => " +
+              time(result.legs[result.legs.length - 1].arrival)
+          );
+
+          if (args.options.descriptive) {
+            result.legs.forEach(leg => {
+              console.log(
+                `${leg.origin.name} (${time(leg.departure)})` +
+                  " => " +
+                  `${leg.destination.name} (${time(leg.arrival)})`
+              );
+            });
+
+            console.log("");
+          }
+        });
 
         callback();
       });
